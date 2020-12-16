067cfa0844bc2d7d520755aeff72d66a18964295
Pranjal Jumde <pranjal@brave.com>
2020-12-16 14:08:16 -0800

    use evaluateSafeJavascript wrappers in TabManager.swift

--- Client/Frontend/Browser/TabManager.swift
+++ Client/Frontend/Browser/TabManager.swift
@@ -900,7 +900,7 @@ extension TabManager: WKNavigationDelegate {
         tab.noImageMode = isNoImageMode
         
         if !tab.contentBlocker.isEnabled {
-            webView.evaluateJavaScript("window.__firefox__.TrackingProtectionStats.setEnabled(false, \(UserScriptManager.securityToken))", completionHandler: nil)
+            webView.evaluateSafeJavascript(functionName: "window.__firefox__.TrackingProtectionStats.setEnabled", args: [false, UserScriptManager.securityToken], sandboxed: false, completion: nil)
         }
     }
 
